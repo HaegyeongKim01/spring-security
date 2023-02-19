f9c287dbcaa7be784f5847166b37294645be5dff
haegyeong <haegyeong.kim01@gmail.com>
2023-02-19 17:29:29 +0900

    로그인 사용자 추가

--- src/main/java/com/prgms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgms/devcourse/configures/WebSecurityConfigure.java
@@ -2,6 +2,7 @@ package com.prgms.devcourse.configures;
 
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 로그인 사용자 추가
+     * @param auth
+     * @throws Exception 예외처리
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN")
+                ;
+    }
 
     /**
      * Override 하여 우리가 원하는대로 security customizing 가능
@@ -30,13 +45,12 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()//공개(인증영역) 리소스 혹은 보호받는 리소스(익명영역)에 대한 세부 설정
-            .antMatchers("/me").hasAnyRole("USER", "ADMIN") //path: me인 경우 요청하는 사용자가 USER 혹은 ADMIN권한을 가지고 있어야 한다.
-            .anyRequest().permitAll()//위의 경우를 제외하고는 모두 permit
-            .and()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN") //path: me인 경우 요청하는 사용자가 USER 혹은 ADMIN권한을 가지고 있어야 한다.
+                .anyRequest().permitAll()//위의 경우를 제외하고는 모두 permit
+               .and()
             .formLogin()
-            .defaultSuccessUrl("/")  //로그인 성공 경우의 path지정
-            .permitAll()
-            .and()
+                .defaultSuccessUrl("/")  //로그인 성공 경우의 path지정
+                 .permitAll()
         ;
     }
 
