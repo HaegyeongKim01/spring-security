3096aa6b576dc7379529b05099050ed8903a813b
haegyeong <haegyeong.kim01@gmail.com>
2023-02-27 20:43:53 +0900

    jpaRespository @Query 사용

--- src/main/java/com/prgms/devcourse/user/UserRepository.java
+++ src/main/java/com/prgms/devcourse/user/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    //Patch Join 이용한 쿼리
+    @Query("select u from User u join fetch u.group g left join fetch g.groupPermissions gp join fetch gp.permission where u.loginId = :loginId")
     Optional<User> findByLoginId(String loginId);
 
 }
